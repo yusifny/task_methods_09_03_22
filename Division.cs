7488cf498452673a973e6057cb3e21fd360afefd
Yusif Nazarbayov (CodeAcademy) <99797435+yusifny@users.noreply.github.com>
2022-03-08 23:49:49 +0400

    file created
    file created

---
+++ Division.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace task_methods_09._03._22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Division(15, 2)); 
+        }
+
+        static double Division(double num, double num2)
+        {
+            double result = num / num2;
+            return result;
+        }
+    }
+}
